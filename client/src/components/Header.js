d1100e09103cde980ff00e3f9ccde8073edd3e90
thomas <tom.angotti11@gmail.com>
2023-05-23 12:02:48 -0600

    updated nav bar button feature

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react"
 
 function Header(){
     const [headerLogo, setHeaderLogo] = useState(null)
+    const [menuVis, setMenuVis] = useState(false)
 
     useEffect(() => {
         fetch('/logoLatest')
@@ -16,17 +17,23 @@ function Header(){
         
     }, [])
 
+    function handleMenu(){
+        setMenuVis(!menuVis)
+    }
+
     return(
     <>
         <div className="header">
                 {headerLogo ? <img src={headerLogo.photo} /> : null}
         </div>
         <div className="navbar">
-            <div className="login">
-                <h4>Bows</h4>
-                <h4>About us</h4>
-                <h4>Contact</h4>
-                <h4>Account</h4>
+            <button onClick={handleMenu}>☰</button>
+            <button id="cartEmoji">🛒</button>
+            <div className={`menu ${menuVis ? "" : "collapsed"}`}>
+                <h4>- Bows</h4>
+                <h4>- About us</h4>
+                <h4>- Contact</h4>
+                <h4>- Account</h4>
             </div>
         </div>
         
